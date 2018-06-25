70df4ca2b7c467e1140f8e9af4d851a212dedfda
JanHuang <bboyjanhuang@gmail.com>
2018-06-25 11:01:38 +0800

    修改路由

--- config/routes.php
+++ config/routes.php
@@ -2,11 +2,11 @@
 
 route()->group('/v1',function (){
     route()->group('/catalog',function (){
-        route()->post('/service','CatalogController@post');
-        route()->delete('/service','CatalogController@delete');
-        route()->patch('/service','CatalogController@patch');
         route()->get('/services', 'CatalogController@list');
-        route()->get('/service/{service}', 'CatalogController@show');
+        route()->get('/services/{service}', 'CatalogController@show');
+        route()->post('/services','CatalogController@post');
+        route()->delete('/services/{service}','CatalogController@delete');
+        route()->patch('/services/{service}','CatalogController@patch');
     });
 });
 
